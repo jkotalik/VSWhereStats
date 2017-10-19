7324521ede5444a2bcfa55207245a69a282ee07a
Justin Kotalik <jukotali@microsoft.com>
2017-10-19 15:47:23 -0700

    adds project

---
+++ VSWhereStats/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace VSWhereStats
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var processStartInfo = new ProcessStartInfo();
+            processStartInfo.Arguments = " -latest -products * -requires Microsoft.Component.MSBuild -property installationPath";
+            processStartInfo.FileName = @"C:\Program Files (x86)\Microsoft Visual Studio\Installer\vswhere.exe";
+            using (var process = Process.Start(processStartInfo))
+            {
+                process.WaitForExit();
+                Console.ReadLine();
+            }
+        }
+    }
+}
